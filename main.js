7cba94b20682c76d6fcdf521a1a717bab4918b5b
Mike Tsao <mike@sowbug.com>
2013-08-08 13:30:33 -0700

    New DOM layout.

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ function newPuzzle(boardDom) {
       var box = board.getBoxByColumnAndRow(i, j);
       box.domElement = document.querySelector("#box-" + box.index);
       box.domElement.box = box;
-      box.text = box.domElement.getElementsByTagName('span')[0];
     }
   }
   var solver = new Solver();
@@ -22,10 +21,10 @@ function newPuzzle(boardDom) {
   for (var i = 0; i < 81; ++i) {
     var box = board.getBox(i);
     if (box.value > 0) {
-      box.text.innerHTML = box.value;
+      box.domElement.innerHTML = box.value;
       box.domElement.classList.add('fixed');
     } else {
-      box.text.innerHTML = '';
+      box.domElement.innerHTML = '';
       box.domElement.onclick = function(e) {
         if (selectedBox >= 0)
           board.getBox(selectedBox).domElement.classList.remove('selected');
@@ -39,28 +38,62 @@ function newPuzzle(boardDom) {
   return board;
 }
 
+function createBoardBox(majorRowIndex, squareIndex,
+  minorRowIndex, boxIndex) {
+  var td = document.createElement("td");
+  var index = majorRowIndex * 27 + squareIndex * 3
+    + minorRowIndex * 9 + boxIndex;
+  td.id = "box-" + index;
+  td.classList.add("box");
+  return td;
+}
+
+function createBoardMinorRow(majorRowIndex, squareIndex, minorRowIndex) {
+  var tr = document.createElement("tr");
+  tr.classList.add("minor-row");
+  for (var i = 0; i < 3; ++i) {
+    tr.appendChild(createBoardBox(majorRowIndex,
+      squareIndex, minorRowIndex, i));
+  }
+  return tr;
+}
+
+function createBoardSquare(rowIndex, squareIndex) {
+  var td = document.createElement("td");
+  td.classList.add("square");
+  var table = document.createElement("table");
+  var tbody = document.createElement("tbody");
+  td.appendChild(table);
+  table.appendChild(tbody);
+  for (var i = 0; i < 3; ++i) {
+    tbody.appendChild(createBoardMinorRow(rowIndex, squareIndex, i));
+  }
+  return td;
+}
+
+function createBoardMajorRow(rowIndex) {
+  var tr = document.createElement("tr");
+  tr.classList.add("major-row");
+  for (var i = 0; i < 3; ++i) {
+    tr.appendChild(createBoardSquare(rowIndex, i));
+  }
+  return tr;
+}
+
+function createBoardTable() {
+  var table = document.createElement("table");
+  table.classList.add("board");
+  var tbody = document.createElement("tbody");
+  table.appendChild(tbody);
+  for (var i = 0; i < 3; ++i) {
+    tbody.appendChild(createBoardMajorRow(i));
+  }
+  return table;
+}
+
 function createBoard() {
   var boardDom = document.querySelector("#board");
-
-  for (var j = 0; j < 9; ++j) {
-    for (var i = 0; i < 9; ++i) {
-      var boxDom = document.createElement('div');
-      var boxIndex = i + j * 9;
-      boxDom.id = 'box-' + boxIndex;
-      boxDom.className = 'box';
-      boxDom.style.position = 'absolute';
-      boxDom.style.left = i * boxWidth + 'px';
-      boxDom.style.top = j * boxHeight + 'px';
-      if ((i + 1) % 3 == 0 && i < 8)
-        boxDom.classList.add('square-right');
-      if ((j + 1) % 3 == 0 && j < 8)
-        boxDom.classList.add('square-bottom');
-      boardDom.appendChild(boxDom);
-      var text = document.createElement('span');
-      text.className = 'text';
-      boxDom.appendChild(text);
-    }
-  }
+  boardDom.appendChild(createBoardTable());
   return boardDom;
 }
 
@@ -114,13 +147,13 @@ onload = function() {
     if (e.keyCode >= 49 && e.keyCode <= 58) {
       var value = e.keyCode - 48;
       changed = board.setBox(selectedBox, value);
-      box.text.innerHTML = value;
+      box.domElement.innerHTML = value;
     }
 
     // Why can't I get the delete key?
     if (e.keyCode == 32 || e.keyCode == 48) {
       changed |= board.clearBox(selectedBox);
-      box.text.innerHTML = '';
+      box.domElement.innerHTML = '';
     }
     if (changed && boardHasProblem) {
       checkBoard();
