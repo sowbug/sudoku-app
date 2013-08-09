0dc7bb733690603cc6466ba581e973427ca84384
Mike Tsao <mike@sowbug.com>
2013-08-08 19:17:37 -0700

    Adjusted window size and made certain inherited styles more explicit.

--- background.js
+++ background.js
@@ -1,9 +1,19 @@
+var WIDTH = 400;
+var HEIGHT = 440;
+
+var onCreate = function(window) {
+    // There seems to be a bug where resizeTo includes the title bar
+    // height and isn't the same as the width/height parameters passed
+    // to app.window.create().
+    //    window.resizeTo(WIDTH, HEIGHT + UNKNOWN_TITLE_BAR_HEIGHT);
+};
+
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('main.html', {
-    'id': 'sudoku',
+    'id': 'sudoku-0.0.3.0-id',
     'frame': 'chrome',
-    'width': 400,
-    'height': 420,
-    'resizable': false
-  });
+    'width': WIDTH,
+    'height': HEIGHT,
+    'resizable': true
+  }, onCreate);
 });
